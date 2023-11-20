fd695e5e2554a6e6080e4154a28f584fba53348e
Pedro Guilherme <pedropgfo@gmail.com>
2023-11-20 16:33:09 -0300

    Aula Pokemon

---
+++ aula_karate/src/test/java/aula_inatel/pokemon/PokeRunner.java
@@ -0,0 +1,13 @@
+package aula_inatel.pokemon;
+
+import com.intuit.karate.junit5.Karate;
+
+class PokeRunner {
+    
+    @Karate.Test
+    Karate testPokemon() {
+        return Karate.run("pokemon").relativeTo(getClass());
+    }    
+
+}
+
